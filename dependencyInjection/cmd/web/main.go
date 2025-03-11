086380be5cd5e8f1542a20571ce4d8a86ca1e8ef
AmilcarVasquez <123046918+amilcar-vasquez@users.noreply.github.com>
2025-03-11 05:05:50 +0000

    added db stuff

--- dependencyInjection/cmd/web/main.go
+++ dependencyInjection/cmd/web/main.go
@@ -1,29 +1,71 @@
 package main
 
 import (
+	"context"
+	"database/sql"
 	"flag"
+	_ "github.com/lib/pq"
+	"html/template"
 	"log/slog"
 	"os"
+	"time"
 )
 
 type application struct {
-	logger *slog.Logger
-	addr   *string
+	addr          *string
+	logger        *slog.Logger
+	templateCache map[string]*template.Template
 }
 
 func main() {
 	addr := flag.String("addr", ":4000", "HTTP network address")
+	dsn := flag.String("dsn", "postgres://feedback:tapirhorse@localhost/feedback?sslmode=disable", "PostgreSQL DSN")
+
 	flag.Parse()
+
 	logger := slog.New(slog.NewTextHandler(os.Stdout, nil))
+	db, err := openDB(*dsn)
+
+	if err != nil {
+		logger.Error(err.Error())
+		os.Exit(1)
+	}
+
+	logger.Info("database connection pool established")
+	if err != nil {
+		logger.Error(err.Error())
+		os.Exit(1)
+	}
+
+	defer db.Close()
+
 	// create an instance of the application struct
 	app := &application{
 		logger: logger,
 		addr:   addr,
 	}
 
-	err := app.serve()
+	err = app.serve()
 	if err != nil {
-		app.logger.Error(err.Error())
+		logger.Error(err.Error())
 		os.Exit(1)
 	}
+}
+
+func openDB(dsn string) (*sql.DB, error) {
+	db, err := sql.Open("postgres", dsn)
+	if err != nil {
+		return nil, err
+	}
+
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+
+	err = db.PingContext(ctx)
+	if err != nil {
+		db.Close()
+		return nil, err
+	}
+
+	return db, nil
 }
